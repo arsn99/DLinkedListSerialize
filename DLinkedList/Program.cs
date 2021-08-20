f954a74f969e93198d4d6219a02f3ebd6da721a9
Кейк Кейк <arsn999@gmail.com>
2021-08-20 17:44:02 +0300

    Добавлено: - Обработка '.','\n','\n\n' - В методе сериализации List заменен на Dictionary, для более быстрого обращения к элементам - Проверку на пустоту списка

--- DLinkedList/Program.cs
+++ DLinkedList/Program.cs
@@ -23,25 +23,39 @@ namespace DLinkedList
             public ListNode Tail;
             public int Count;
 
+            public ListRandom()
+            {
+
+            }
             public ListRandom(Stream s)
             {
                 Deserialize(s);
             }
             public ListRandom(string data)
             {
-                Head = Tail = new ListNode();
-                Head.Data = data;
-                Head.Next = null;
-                Count++;
+                //Head = Tail = new ListNode();
+                //Head.Data = data;
+                //Head.Next = null;
+                //Count++;
+                AddNode(data);
             }
             public void AddNode(string data)
             {
                 ListNode node = new ListNode();
                 node.Data = data;
-                node.Previous = Tail;
-                node.Next = null;
-                Tail.Next = node;
-                Tail = node;
+
+                if (Head==null)
+                {
+                    Head = Tail = node;
+                    node.Previous = null;
+                }
+                else
+                {
+                    node.Previous = Tail;
+                    Tail.Next = node;
+                    Tail = node;
+                }
+                node.Next = null;                
                 Count++;
             }
             public void SetRandom()
@@ -61,47 +75,78 @@ namespace DLinkedList
             }
             public void Serialize(Stream s)
             {
-                List<ListNode> indexList = new List<ListNode>();
-                for (ListNode node = Head; node != null; node = node.Next)
-                    indexList.Add(node);
-                
-                using (StreamWriter writer = new StreamWriter(s))
+                if (Head != null)
                 {
-                    foreach (ListNode node in indexList)
-                        writer.WriteLine(node.Data+"."+indexList.IndexOf(node.Random).ToString());
+                    Dictionary<ListNode, int> indexList = new Dictionary<ListNode, int>();
+                    //List<ListNode> indexList = new List<ListNode>();
+                    int i = 0;
+                    for (ListNode node = Head; node != null; node = node.Next,i++)
+                        indexList.Add(node,i);
+
+                    using (StreamWriter writer = new StreamWriter(s))
+                    {
+                        for (ListNode node = Head; node!=null; node = node.Next)
+                        {
+                            writer.WriteLine($"{node.Data}.{indexList[node.Random]}");
+                        }
+                            
+                    }
                 }
             }
             public void Deserialize(Stream s)
             {
                 Count = 0;
-                ListNode node = new ListNode();
-                Head = node;
+                //ListNode node = new ListNode();
+                //Head = node;
                 List<ListNode> indexList = new List<ListNode>();
 
                 using (StreamReader reader = new StreamReader(s))
                 {
+                    
                     string line;
+                    string temp=String.Empty;
+                    
                     while ((line = reader.ReadLine())!=null)
                     {
-                        if (!line.Equals(""))
+                        if (line=="")
                         {
-                            node.Data = line;
-                            ListNode next = new ListNode();
-                            node.Next = next;
-                            next.Previous = node;
-                            indexList.Add(node);
-                            node = next;
-                            Count++;
+                            continue;
+                        }
+                        else
+                        if (!line.Contains('.')&&temp==String.Empty)
+                        {
+                            temp = line;
+                        }
+                        else
+                        {
+                            //node.Data = $"{temp}.{line}";
+                            if (temp==string.Empty)
+                                AddNode(line);
+                            else
+                            {
+                                AddNode($"{temp}.{line}");
+                                temp = String.Empty;
+                            }
+                            indexList.Add(Tail);
+                            //ListNode next = new ListNode();
+                            //node.Next = next;
+                            //next.Previous = node;
+                            //indexList.Add(node);
+                            //node = next;
+                            //Count++;
                         }
                     }
                 }
-                Tail = node.Previous;
-                Tail.Next = null;
-                foreach (ListNode item in indexList)
+                if (Head != null)
                 {
-                    var tempString = item.Data.Split('.');
-                    item.Random = indexList[int.Parse(tempString[1])];
-                    item.Data = tempString[0];
+                    //Tail = node.Previous;
+                    //Tail.Next = null;
+                    foreach (ListNode item in indexList)
+                    {
+                        var tempString = item.Data.Split('.');
+                        item.Random = indexList[int.Parse(tempString[1])];
+                        item.Data = tempString[0];
+                    }
                 }
             }
         }
@@ -109,7 +154,7 @@ namespace DLinkedList
        
         static void Main(string[] args)
         {
-            ListRandom listRandom = new ListRandom("Head Test");
+            ListRandom listRandom = new ListRandom();//new ListRandom("Head Test");
             Random random = new Random();
             for (int i = 0; i < 5/*random.Next(5,10)*/; i++)
             {
@@ -117,7 +162,7 @@ namespace DLinkedList
             }
             listRandom.SetRandom();
 
-            FileStream file = new FileStream("list.dat", FileMode.OpenOrCreate); 
+            FileStream file = new FileStream("list.dat", FileMode.Create); 
             listRandom.Serialize(file);
             file.Close();
 
@@ -133,7 +178,7 @@ namespace DLinkedList
                 ListNode first = listRandom.Head;
                 ListNode second = listRandom2.Head;
 
-                for (; first != null || second != null; first = first.Next, second = second.Next)
+                for (; first != null && second != null; first = first.Next, second = second.Next)
                 {
                     if ((first.Data != second.Data) || (first.Random.Data != second.Random.Data))
                         throw new Exception("Wrong Data");
